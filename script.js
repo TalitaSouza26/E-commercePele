30771b1c0a0f089cc7f83476dc3a80d5708c587e
DevTalita <taa.jesus09@gmail.com>
2024-01-27 02:55:39 -0300

    E-commerce Cosmetics

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const navheader = document.querySelector(".navheader");
+
+menu.addEventListener("click", function() {
+    menu.classList.toggle("change");
+    navheader.style.display = (navheader.style.display === "block") ? "none" : "block";
+});
